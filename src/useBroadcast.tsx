d50a0cb9652049e2a6232ae25ec28bb3232af519
Stephen Collins <stephenc211@gmail.com>
2021-10-03 14:06:08 -0500

    added channel namespacing

--- src/useBroadcast.tsx
+++ src/useBroadcast.tsx
@@ -13,8 +13,8 @@ const useBroadcast = (
   const bc = useRef<BroadcastChannel>()
   useEffect(() => {
     const onMessage = (event: MessageEvent<any>) => {
-      const selfContext = sessionStorage.getItem("bc_self_context") || ""
-      const bcContext = localStorage.getItem("bc_context") || ""
+      const selfContext = sessionStorage.getItem(`${channelName}_bc_self_context`) || ""
+      const bcContext = localStorage.getItem(`${channelName}_bc_context`) || ""
       if (selfContext === event.data.id) {
         return
       }
@@ -27,35 +27,35 @@ const useBroadcast = (
     // for receiving from other contexts *ONLY*. Will return early if this context is the emitting context
     // creating this abstraction for "parent" and "child" context, to delineate message handling
     // basically, do not want receiving a message in a child context to update state (unless explicitly...) in the parent
-    let bcContext = localStorage.getItem("bc_context") || ""
-    let selfContext = sessionStorage.getItem("bc_self_context") || ""
+    let bcContext = localStorage.getItem(`${channelName}_bc_context`) || ""
+    let selfContext = sessionStorage.getItem(`${channelName}_bc_self_context`) || ""
     if (!selfContext) {
       if (!bcContext) {
-        localStorage.setItem("bc_context", "1")
-        sessionStorage.setItem("bc_self_context", "1")
+        localStorage.setItem(`${channelName}_bc_context`, "1")
+        sessionStorage.setItem(`${channelName}_bc_self_context`, "1")
       }
     }
     if (!selfContext && bcContext === "1") {
-      sessionStorage.setItem("bc_self_context", `${parseInt(bcContext) + 1}`)
+      sessionStorage.setItem(`${channelName}_bc_self_context`, `${parseInt(bcContext) + 1}`)
     }
     if (!selfContext && !bcContext) {
-      sessionStorage.setItem("bc_self_context", `${1}`)
+      sessionStorage.setItem(`${channelName}_bc_self_context`, `${1}`)
     }
-    selfContext = sessionStorage.getItem("bc_self_context") || ""
-    bcContext = localStorage.getItem("bc_context") || ""
+    selfContext = sessionStorage.getItem(`${channelName}_bc_self_context`) || ""
+    bcContext = localStorage.getItem(`${channelName}_bc_context`) || ""
     bc.current = new BroadcastChannel(channelName)
     bc.current.onmessage = (event) => onMessage(event)
     return () => {
       if (bcContext === "1" && selfContext === "1") {
-        localStorage.removeItem("bc_context")
+        localStorage.removeItem(`${channelName}_bc_context`)
       }
-      sessionStorage.removeItem("bc_self_context")
+      sessionStorage.removeItem(`${channelName}_bc_self_context`)
       bc.current?.close()
       bc.current = undefined
     }
   }, [messages, onMessageCB, channelName])
   const emit = (message: string) => {
-    const selfContext = sessionStorage.getItem("bc_self_context") || ""
+    const selfContext = sessionStorage.getItem(`${channelName}_bc_self_context`) || ""
     bc.current?.postMessage({
       message,
       id: selfContext,
